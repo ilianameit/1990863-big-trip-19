d399af37e165be07cad1014bc2fc5538ce83a4e7
ilianameit <ilianameit@gmail.com>
2023-02-11 18:45:58 +0300

    фильтры

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-//import {render} from './framework/render.js';
+import {render} from './framework/render.js';
 import FiltersPresenter from './presenter/filters-point-presenter.js';
 import PointModel from './model/point-model.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import FilterModel from './model/filter-model.js';
+import FiltersView from './view/filters-view.js';
+
+const filters = [
+  {
+    type: 'everything',
+    name: 'EVERYTHING',
+    count: 0,
+  },
+];
 
 const siteMainElement = document.querySelector('.page-main');
 const siteMainTripEvents = siteMainElement.querySelector('.trip-events');
@@ -13,6 +23,7 @@ const siteHeaderFilterElement = siteHeaderElement.querySelector('.trip-controls_
 //render(new FiltersView(), siteHeaderFilterElement);
 
 const pointModel = new PointModel();
+const filterModel = new FilterModel();
 
 const pointPresenter = new BoardPresenter({
   pointListContainer: siteMainTripEvents,
@@ -21,6 +32,11 @@ const pointPresenter = new BoardPresenter({
 pointPresenter.init();
 const tripPresenter = new TripPresenter({tripContainer: siteTrip}, pointModel);
 tripPresenter.init();
-const filtersPresenter = new FiltersPresenter({filterContainer: siteHeaderFilterElement}, pointModel);
-filtersPresenter.init();
+render(new FiltersView({
+  filters,
+  currentFilterType: 'everything',
+  onFilterTypeChange: () => {}
+}), siteHeaderFilterElement);
+//const filtersPresenter = new FiltersPresenter({filterContainer: siteHeaderFilterElement}, pointModel);
+//filtersPresenter.init();
 
